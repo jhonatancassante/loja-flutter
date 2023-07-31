275a7540228dc60652bc7fb4b79e8ba1316049b1
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-31 16:21:29 -0300

    Criada classe ItemCarrinho

---
+++ lib/models/item_carrinho.dart
@@ -0,0 +1,15 @@
+class ItemCarrinho {
+  final String id;
+  final String idProduto;
+  final String nomeProduto;
+  final int quantidade;
+  final double preco;
+
+  ItemCarrinho({
+    required this.id,
+    required this.idProduto,
+    required this.nomeProduto,
+    required this.quantidade,
+    required this.preco,
+  });
+}
