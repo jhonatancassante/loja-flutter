1fd962f634e1b0ad5fdfb9a3c0919c97b94f5656
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-07-31 16:21:40 -0300

    Criada classe Carrinho

---
+++ lib/models/carrinho.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'produto.dart';
+import 'item_carrinho.dart';
+
+class Carrinho with ChangeNotifier {
+  Map<String, ItemCarrinho> _itens = {};
+
+  Map<String, ItemCarrinho> get itens {
+    return {..._itens};
+  }
+
+  int get contadorItens => _itens.length;
+
+  double get total {
+    double total = 0.0;
+
+    _itens.forEach((key, value) {
+      total += value.preco * value.quantidade;
+    });
+
+    return total;
+  }
+
+  void adicionarItem(Produto produto) {
+    if (_itens.containsKey(produto.id)) {
+      _itens.update(
+        produto.id,
+        (itemExistente) => ItemCarrinho(
+          id: itemExistente.id,
+          idProduto: itemExistente.idProduto,
+          nomeProduto: itemExistente.nomeProduto,
+          quantidade: itemExistente.quantidade + 1,
+          preco: itemExistente.preco,
+        ),
+      );
+    } else {
+      _itens.putIfAbsent(
+        produto.id,
+        () => ItemCarrinho(
+          id: Random().nextDouble().toString(),
+          idProduto: produto.id,
+          nomeProduto: produto.nome,
+          quantidade: 1,
+          preco: produto.preco,
+        ),
+      );
+    }
+    notifyListeners();
+  }
+
+  void removerItem(String idProduto) {
+    _itens.remove(idProduto);
+
+    notifyListeners();
+  }
+
+  void limpar() {
+    _itens = {};
+
+    notifyListeners();
+  }
+}
