b7fa7402bfd4255222699743824eb67386c255e8
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-08-16 20:06:13 -0300

    Criada TelaCarrinho

---
+++ lib/screens/tela_carrinho.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TelaCarrinho extends StatelessWidget {
+  const TelaCarrinho({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Carrinho'),
+      ),
+    );
+  }
+}
