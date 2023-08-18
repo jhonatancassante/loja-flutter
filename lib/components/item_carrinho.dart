6162fd8ae6258e8bde76f8e12931dde5a1c6bb2d
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2023-08-17 21:01:09 -0300

    Implementado dismissible no itemCarrinho

--- lib/components/item_carrinho.dart
+++ lib/components/item_carrinho.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:loja_flutter/models/carrinho.dart';
 import 'package:loja_flutter/models/item_carrinho.dart';
+import 'package:provider/provider.dart';
 
 class ItemCarrinhoWidget extends StatelessWidget {
   const ItemCarrinhoWidget(this.itemCarrinho, {super.key});
@@ -8,31 +10,53 @@ class ItemCarrinhoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    return Dismissible(
+      key: ValueKey(itemCarrinho.id),
+      direction: DismissDirection.endToStart,
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Theme.of(context).colorScheme.primary,
-            foregroundColor: Colors.white,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: FittedBox(
-                child: Text(
-                  '${itemCarrinho.preco}',
+      onDismissed: (_) => Provider.of<Carrinho>(
+        context,
+        listen: false,
+      ).removerItem(itemCarrinho.idProduto),
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Theme.of(context).colorScheme.primary,
+              foregroundColor: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text(
+                    '${itemCarrinho.preco}',
+                  ),
                 ),
               ),
             ),
+            title: Text(itemCarrinho.nomeProduto),
+            subtitle: Text(
+              'Total: R\$ ${itemCarrinho.preco * itemCarrinho.quantidade}',
+            ),
+            trailing: Text("${itemCarrinho.quantidade}x"),
           ),
-          title: Text(itemCarrinho.nomeProduto),
-          subtitle: Text(
-            'Total: R\$ ${itemCarrinho.preco * itemCarrinho.quantidade}',
-          ),
-          trailing: Text("${itemCarrinho.quantidade}x"),
         ),
       ),
     );
