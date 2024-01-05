21e16212d9985823a41a343b5dcb02a21d302946
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-05 16:22:59 -0300

    Adiciona componente AppDrawer

---
+++ lib/components/app_drawer.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:loja_flutter/utils/rotas.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          AppBar(
+            title: const Text('Bem vindo usuário!'),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Loja'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                Rotas.home,
+              );
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Pedidos'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                Rotas.pedidos,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
