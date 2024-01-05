40f28410da80fcf330c92ddf964afce211d0167e
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-05 16:22:43 -0300

    Adiciona tela Pedidos

---
+++ lib/screens/tela_pedidos.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TelaPedidos extends StatelessWidget {
+  const TelaPedidos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meus Pedidos'),
+      ),
+    );
+  }
+}
