40f49fbf70ef5adcc1d790c222be9cb2210a1de9
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-07 15:58:20 -0300

    Adicionada confirmação ao remover item no carrinho

--- lib/components/item_carrinho.dart
+++ lib/components/item_carrinho.dart
@@ -28,6 +28,29 @@ class ItemCarrinhoWidget extends StatelessWidget {
           size: 40,
         ),
       ),
+      confirmDismiss: (_) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: const Text('Tem certeza?'),
+            content: const Text('Quer realmente remover o item do carrinho?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+                child: const Text('Não'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+                child: const Text('Sim'),
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (_) => Provider.of<Carrinho>(
         context,
         listen: false,
