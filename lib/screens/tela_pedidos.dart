eb545de108b765f648f740e6077625a1c9567326
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-07 10:33:53 -0300

    Implementação da tela Pedidos

--- lib/screens/tela_pedidos.dart
+++ lib/screens/tela_pedidos.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:loja_flutter/components/app_drawer.dart';
+import 'package:loja_flutter/components/pedido.dart';
+import 'package:loja_flutter/models/lista_pedido.dart';
+import 'package:provider/provider.dart';
 
 class TelaPedidos extends StatelessWidget {
   const TelaPedidos({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ListaPedido pedidos = Provider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: pedidos.contagemItens,
+        itemBuilder: (ctx, i) => PedidoWidget(
+          pedido: pedidos.itens[i],
+        ),
       ),
     );
   }
