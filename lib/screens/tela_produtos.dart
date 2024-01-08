f7526d702da9a1653111e1ad0e4c33f6b5330307
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-07 21:11:17 -0300

    Adicionada TelaProdutos

--- lib/screens/tela_produtos.dart
+++ lib/screens/tela_produtos.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:loja_flutter/components/app_drawer.dart';
+import 'package:loja_flutter/components/item_produto.dart';
 import 'package:loja_flutter/models/lista_produto.dart';
+import 'package:loja_flutter/utils/rotas.dart';
 import 'package:provider/provider.dart';
 
 class TelaProdutos extends StatelessWidget {
@@ -12,14 +14,27 @@ class TelaProdutos extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Gerenciar produtos.'),
+        title: const Text('Gerenciar Produtos'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(Rotas.formularioProduto);
+            },
+          ),
+        ],
       ),
       drawer: const AppDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
           itemCount: produtos.contagemItens,
-          itemBuilder: (ctx, i) => Text(produtos.itens[i].nome),
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              ItemProduto(produtos.itens[i]),
+              const Divider(),
+            ],
+          ),
         ),
       ),
     );
