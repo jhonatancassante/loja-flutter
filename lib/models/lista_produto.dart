279c5977653fddebf343afbc5bf4754fc1cb9f90
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-11 10:38:21 -0300

    Criados metódos de atualizar, adicionar e remover itens

--- lib/models/lista_produto.dart
+++ lib/models/lista_produto.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import '../data/dados_ficticios.dart';
 import '../models/produto.dart';
@@ -13,10 +15,51 @@ class ListaProduto with ChangeNotifier {
       )
       .toList();
 
-  void adicionarProduto(Produto produto) {
-    _itens.add(produto);
+  void salvarProduto(Map<String, Object> dados) {
+    bool temId = dados['id'] != null;
+
+    final produto = Produto(
+      id: temId ? dados['id'] as String : Random().nextDouble().toString(),
+      nome: dados['nome'] as String,
+      descricao: dados['descricao'] as String,
+      preco: dados['preco'] as double,
+      imagemUrl: dados['imagemUrl'] as String,
+    );
+
+    if (temId) {
+      atualizarProduto(produto);
+    } else {
+      adicionarProduto(produto);
+    }
 
     notifyListeners();
+  }
+
+  void adicionarProduto(Produto produto) {
+    _itens.add(produto);
+  }
+
+  void atualizarProduto(Produto produto) {
+    int index = _itens.indexWhere(
+      (element) => element.id == produto.id,
+    );
+
+    if (index >= 0) {
+      _itens[index] = produto;
+    }
+  }
+
+  void removerProduto(Produto produto) {
+    int index = _itens.indexWhere(
+      (element) => element.id == produto.id,
+    );
+
+    if (index >= 0) {
+      _itens.removeWhere(
+        (element) => element.id == produto.id,
+      );
+      notifyListeners();
+    }
   }
 
   int get contagemItens {
