150c56144a1eb2c53f8c7fea697f66c02778dc47
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-15 12:12:09 -0300

    Implemntado pedidos no banco de dados na nuvem

--- lib/models/lista_pedido.dart
+++ lib/models/lista_pedido.dart
@@ -1,6 +1,8 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:loja_flutter/constants/url_base_db.dart';
 import 'package:loja_flutter/models/carrinho.dart';
 import 'package:loja_flutter/models/pedido.dart';
 
@@ -15,13 +17,35 @@ class ListaPedido with ChangeNotifier {
     return _itens.length;
   }
 
-  void adicionaPedido(Carrinho carrinho) {
+  Future<void> adicionaPedido(Carrinho carrinho) async {
+    final data = DateTime.now();
+    final resposta = await http.post(
+      Uri.parse('$urlBaseDb/pedidos.json'),
+      body: jsonEncode(
+        {
+          'total': carrinho.total,
+          'data': data.toIso8601String(),
+          'produtos': carrinho.itens.values
+              .map(
+                (item) => {
+                  'id': item.id,
+                  'idProduto': item.idProduto,
+                  'nomeProduto': item.nomeProduto,
+                  'quantidade': item.quantidade,
+                  'preco': item.preco,
+                },
+              )
+              .toList(),
+        },
+      ),
+    );
+    final id = jsonDecode(resposta.body)['name'];
     _itens.insert(
       0,
       Pedido(
-        id: Random().nextDouble().toString(),
+        id: id,
         total: carrinho.total,
-        data: DateTime.now(),
+        data: data,
         produtos: carrinho.itens.values.toList(),
       ),
     );
