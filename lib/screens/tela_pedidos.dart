1412ebd61dbe608d44c2a5a1074515f33e4dcbd0
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-15 14:43:08 -0300

    Atualizado tele de pedidos para utiliza FutureBuilder

--- lib/screens/tela_pedidos.dart
+++ lib/screens/tela_pedidos.dart
@@ -6,47 +6,43 @@ import 'package:provider/provider.dart';
 
 import '../utils/atualiza_lista_pedidos.dart';
 
-class TelaPedidos extends StatefulWidget {
+class TelaPedidos extends StatelessWidget {
   const TelaPedidos({super.key});
 
   @override
-  State<TelaPedidos> createState() => _TelaPedidosState();
-}
-
-class _TelaPedidosState extends State<TelaPedidos> {
-  bool _estaCarregando = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<ListaPedido>(
-      context,
-      listen: false,
-    ).carregarPedidos().then((_) {
-      setState(() => _estaCarregando = false);
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ListaPedido pedidos = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: _estaCarregando
-          ? const Center(child: CircularProgressIndicator())
-          : RefreshIndicator(
+      body: FutureBuilder(
+        future: Provider.of<ListaPedido>(
+          context,
+          listen: false,
+        ).carregarPedidos(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text('Ocorreu um erro ao carregar a lista de produtos.'),
+            );
+          } else {
+            return RefreshIndicator(
               onRefresh: () => atualizarListaPedidos(context),
-              child: ListView.builder(
-                itemCount: pedidos.contagemItens,
-                itemBuilder: (ctx, i) => PedidoWidget(
-                  pedido: pedidos.itens[i],
+              child: Consumer<ListaPedido>(
+                builder: (ctx, pedidos, child) => ListView.builder(
+                  itemCount: pedidos.contagemItens,
+                  itemBuilder: (ctx, i) => PedidoWidget(
+                    pedido: pedidos.itens[i],
+                  ),
                 ),
               ),
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
