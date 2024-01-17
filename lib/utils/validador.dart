6f715ae998f1dbb510da920a70c7a07068a6fb2b
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-17 13:42:43 -0300

    Implementado validadores de email, senha e confirmar senha

--- lib/utils/validador.dart
+++ lib/utils/validador.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class Validador {
   static String? campoString(String? s, int minChar) {
     final sTemp = s ?? '';
@@ -29,6 +31,57 @@ class Validador {
 
     if (!eValida) return "A URL não é válida!";
     if (!terminaComArquivo) return "O tipo de arquivo informado não é válido.";
+    return null;
+  }
+
+  static String? campoEmail(String? email) {
+    final emailTemp = email ?? '';
+    String pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
+    RegExp regExp = RegExp(pattern);
+
+    if (emailTemp.trim().isEmpty) {
+      return 'Campo de email vazio.';
+    }
+
+    if (!regExp.hasMatch(emailTemp)) {
+      return 'E-mail inválido.';
+    }
+
+    return null;
+  }
+
+  static String? campoSenha(String? senha) {
+    final senhaTemp = senha ?? '';
+    String pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[_%$#@])$';
+    RegExp regExp = RegExp(pattern);
+
+    if (senhaTemp.trim().isEmpty) {
+      return 'Campo de senha vazio.';
+    }
+
+    if (senhaTemp.length < 8) {
+      return 'Senha muito curta. Deve conter ao menos 8 dígitos.';
+    }
+
+    if (!regExp.hasMatch(senhaTemp)) {
+      return '''Sua senha deve ter:
+      • Pelo menos uma letra maiúscula ([A-Z])
+      • Pelo menos uma letra minúscula ([a-z])
+      • Pelo menos um dígito ([0-9])
+      • Pelo menos um caractere especial (_%\$#@)''';
+    }
+
+    return null;
+  }
+
+  static String? campoConfirmaSenha(
+    String? senha,
+    TextEditingController controlador,
+  ) {
+    final confirmaSenha = senha ?? '';
+    if (confirmaSenha != controlador.text) {
+      return 'A confirmação de senha não coincide.';
+    }
     return null;
   }
 }
