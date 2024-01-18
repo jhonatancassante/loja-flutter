721345ecae8e1b577987f4706b466b726d3d36b0
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-17 23:17:14 -0300

    Criada exceções de autenticação

---
+++ lib/errors/autenticacao_excecao.dart
@@ -0,0 +1,20 @@
+class AutenticacaoExcecao implements Exception {
+  static const Map<String, String> erros = {
+    'EMAIL_EXISTS': 'E-mail já cadastrado.',
+    'OPERATION_NOT_ALLOWED': 'Operação não permitida.',
+    'TOO_MANY_ATTEMPTS_TRY_LATER':
+        'Acesso bloqueado por muitas tentativas. Tente mais tarde.',
+    'EMAIL_NOT_FOUND': 'E-mail não encontrado.',
+    'INVALID_PASSWORD': 'Senha inválida.',
+    'USER_DISABLED': 'A conta do usuário foi desabilitada.',
+  };
+
+  final String key;
+
+  const AutenticacaoExcecao(this.key);
+
+  @override
+  String toString() {
+    return erros['key'] ?? 'Ocorreu um erro no processo de autenticação.';
+  }
+}
