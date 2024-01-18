a0b1d324f30bc1b76b486a4de8fc141b95dbdd6c
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-17 23:06:11 -0300

    Implementado métodos de Sign In e Sign Up

--- lib/models/autenticacao.dart
+++ lib/models/autenticacao.dart
@@ -6,11 +6,9 @@ import 'package:loja_flutter/configs/api_key.dart';
 import 'package:loja_flutter/constants/url_base_autenticacao.dart';
 
 class Autenticacao with ChangeNotifier {
-  static const _url = '$urlBaseAutenticacao:signUp?key=$apiKey';
-
-  Future<void> signup(String email, String senha) async {
+  Future<void> _autenticar(String email, String senha, String tipoUrl) async {
     final resposta = await http.post(
-      Uri.parse(_url),
+      Uri.parse('$urlBaseAutenticacao:$tipoUrl?key=$apiKey'),
       body: jsonEncode({
         'email': email,
         'password': senha,
@@ -19,5 +17,13 @@ class Autenticacao with ChangeNotifier {
     );
 
     print(jsonDecode(resposta.body));
+  }
+
+  Future<void> signUp(String email, String senha) async {
+    _autenticar(email, senha, 'signUp');
+  }
+
+  Future<void> signIn(String email, String senha) async {
+    _autenticar(email, senha, 'signInWithPassword');
   }
 }
