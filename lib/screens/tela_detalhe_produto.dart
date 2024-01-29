4743cdfc2d6133a2c6c3707898a370a3efd0be84
Jhonatan Cassante de Brito <jhonatan.cassante@live.com>
2024-01-29 16:19:50 -0300

    Melhorias de contraste

--- lib/screens/tela_detalhe_produto.dart
+++ lib/screens/tela_detalhe_produto.dart
@@ -10,47 +10,94 @@ class TelaDetalheProduto extends StatelessWidget {
     final Produto produto =
         ModalRoute.of(context)!.settings.arguments as Produto;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(produto.nome),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              width: double.infinity,
-              margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-              child: Hero(
-                tag: produto.id,
-                child: Image.network(
-                  produto.imagemUrl,
-                  fit: BoxFit.cover,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            iconTheme: IconThemeData(
+              color: Colors.white,
+              opacity: 1.0,
+              size: 30,
+              shadows: List.of(
+                <Shadow>[
+                  const Shadow(
+                    color: Colors.black,
+                    blurRadius: 2,
+                  ),
+                  const Shadow(
+                    color: Colors.black,
+                    blurRadius: 5,
+                  ),
+                  const Shadow(
+                    color: Colors.black,
+                    blurRadius: 10,
+                  ),
+                  const Shadow(
+                    color: Colors.black,
+                    blurRadius: 15,
+                  ),
+                ],
+              ),
+            ),
+            shadowColor: Colors.red,
+            expandedHeight: 300,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(produto.nome),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Hero(
+                    tag: produto.id,
+                    child: Image.network(
+                      produto.imagemUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment(0, 0.8),
+                        end: Alignment(0, 0),
+                        colors: [
+                          Color.fromRGBO(0, 0, 0, 0.6),
+                          Color.fromRGBO(0, 0, 0, 0),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate(
+              [
+                Text(
+                  'R\$ ${Formatar.moeda(
+                    produto.preco,
+                  )}',
+                  style: const TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  width: double.infinity,
+                  child: Text(
+                    produto.descricao,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'R\$ ${Formatar.moeda(
-                produto.preco,
-              )}',
-              style: const TextStyle(
-                color: Colors.black87,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              padding: const EdgeInsets.symmetric(
-                horizontal: 10,
-              ),
-              width: double.infinity,
-              child: Text(
-                produto.descricao,
-                textAlign: TextAlign.center,
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
